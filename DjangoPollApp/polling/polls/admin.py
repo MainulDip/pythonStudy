d2204dba77451994c490506cfb6c2f47d0eff8bf
mainuldip <mainuldip@gmail.com>
2022-07-06 01:17:29 -0400

    custom options adding in the admin page for App's entry

--- DjangoPollApp/polling/polls/admin.py
+++ DjangoPollApp/polling/polls/admin.py
@@ -3,5 +3,20 @@ from .models import Question, Choice
 
 # Register your models here.
 
-admin.site.register([Question, Choice])
+# admin.site.register([Question, Choice])
 # admin.site.register(Choice)
+
+class ChoiceInline(admin.TabularInline): # admin.StackedInline for vertical listing
+    model = Choice
+    extra = 0 # how many extra row after lisitng all options
+class QuestionAdmin(admin.ModelAdmin):
+    # fields = ['pub_date', 'question_text']
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+
+    inlines = [ChoiceInline]
+
+
+admin.site.register(Question, QuestionAdmin)
