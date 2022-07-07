b91e9a74b53b96e5b23da437c9f0a7a57b87491d
mainuldip <mainuldip@gmail.com>
2022-07-07 01:12:30 -0400

    template folder init

--- DjangoPollApp/polling/polls/views.py
+++ DjangoPollApp/polling/polls/views.py
@@ -1,6 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Question, Choice
 
 # Create your views here.
+# def index(request):
+#     return HttpResponse("Hello, world. You're at the polls index.")
+
+# Get Questions and Dispay
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, 'polls/index.html')
